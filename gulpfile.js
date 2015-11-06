bb658a352efdf28176e19ef3b2ce3bd03c185520
Eric Bower <neurosnap@gmail.com>
2015-11-06 12:20:54 -0500

    tester do

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,85 @@
 'use strict';
 
+var fs = require('fs');
 var gulp = require('gulp');
-var requireDir = require('require-dir');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var gutil = require('gulp-util');
+var flatten = require('gulp-flatten');
 
-requireDir('./tasks', { recurse: true });
+gulp.task('client', function(done) {
+  bundleFiles(done);
+});
 
-// Default task
-gulp.task('default', ['sass', 'babel', 'browserify']);
+gulp.task('watch', function(done) {
+  bundleFiles(done, true);
+});
+
+/**
+ * Gets all .js files, non-recursively and then creates separate browserify bundles
+ */
+function bundleFiles(done, watch, src) {
+  if (typeof src === 'undefined') src = './src/client/';
+
+  var count = 0;
+  var files = fs.readdirSync(src);
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    if (file.indexOf('.swp') >= 0) continue;
+    if (file.toLowerCase().indexOf('.js' >= 0)) {
+      bundle(callbacksDone, watch, src + file);
+    }
+  }
+
+  function callbacksDone() {
+    count++;
+    if (count == files.length) done();
+  }
+}
+
+/**
+ * Bundles a single js file into a browserify bundle
+ */
+function bundle(done, watch, fname) {
+  gutil.log('Browserify bundling ' + fname);
+
+  var browserifyOpts = Object.assign({}, watchify.args, { debug: true });
+  var babelOpts = {
+    presets: ['es2015', 'react', 'stage-0'],
+    plugins: ['transform-runtime'],
+  };
+
+  var b = browserify(fname, browserifyOpts);
+  // place all transforms here
+  b.transform('babelify', babelOpts);
+
+  if (watch) {
+    var w = watchify(b);
+    gutil.log('Watching ' + fname);
+    w.on('log', gutil.log);
+    w.on('update', function() {
+      bundle(undefined, false, fname);
+    });
+  }
+
+  b.bundle()
+    .on('error', gutil.log)
+    .pipe(source(fname))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(flatten())
+    .pipe(gulp.dest('./public/js'))
+    .on('end', function() {
+      gutil.log('Browserify finishined bundling ' + fname);
+      if (typeof done === 'function') done();
+    });
+
+  return b;
+}
+
 
